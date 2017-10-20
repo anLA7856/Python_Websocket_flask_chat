2a70862a8ee7b63c059cd3873dce34729de19248
anLA7856 <350493391@qq.com>
2017-10-21 00:04:38 +0800

    move testCase to testServer.py update readme.md

--- class/_chat.py
+++ class/_chat.py
@@ -1,15 +1,5 @@
+# coding=UTF-8
 '''
 my first py    
 '''
-#初始化
-from flask import Flask
-app = Flask(__name__)
 
-#路由和视图函数
-@app.route('/')
-def index():
-    return '<h1> Hello World!</h1>'
-
-#启动服务器，默认ip端口为127.0.0.1:5000
-if __name__ == '__main__':
-    app.run(debug=True)
